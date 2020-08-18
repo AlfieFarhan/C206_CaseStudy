d77712f0759c9506b054ea49057196c96b2ebc02
19015268 <19015268@LAPTOP-9V6S919R>
2020-08-18 12:59:41 +0800

    Updated searchRateByCurrency() and convertCurrency()

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -6,24 +6,24 @@ public class C206_CaseStudy {
 		// TODO Auto-generated method stub
 		ArrayList<MoneyHolding> MoneyHoldingList = new ArrayList<MoneyHolding>();
 		ArrayList<Currency> currencyList = new ArrayList<Currency>();
-		Currency MYR = new Currency("MYR", 3.06, 3.06);
-		Currency USD = new Currency("USD", 0.73, 0.73);
-		Currency THB = new Currency("THB", 22.88, 22.7);
-		Currency TWD = new Currency("TWD", 22.57, 22.25);
-		Currency CNY = new Currency("CNY", 5.09, 5.07);
-		Currency HKD = new Currency("HKD", 5.68, 5.65);
-		Currency JPY = new Currency("JPY", 78.01, 77.04);
-		Currency KRW = new Currency("KRW", 878.73, 874.15);
-		Currency EUR = new Currency("EUR", 0.62, 0.62);
-		Currency GBP = new Currency("GBP", 0.56, 0.56);
-		Currency AUD = new Currency("AUD", 1.03, 1.02);
-		Currency NZD = new Currency("NZD", 1.12, 1.10);
-		Currency IDR = new Currency("IDR", 10638.30, 10250);
-		Currency PHP = new Currency("PHP", 38.76, 37.2);
-		Currency VND = new Currency("VND", 16666.67, 0);
-		Currency INR = new Currency("INR", 51.81, 0);
-		Currency CHF = new Currency("CHF", 0.67, 0);
-		Currency CAD = new Currency("CAD", 0.97, 0.96);
+		Currency MYR = new Currency("MYR","Malaysian Ringgit", 3.06, 3.06);
+		Currency USD = new Currency("USD", "US Dollar", 0.73, 0.73);
+		Currency THB = new Currency("THB", "Thai Baht", 22.88, 22.7);
+		Currency TWD = new Currency("TWD", "Taiwan Dollar", 22.57, 22.25);
+		Currency CNY = new Currency("CNY", "Chinese Yuan(RMB)", 5.09, 5.07);
+		Currency HKD = new Currency("HKD", "Hong Kong Dollar", 5.68, 5.65);
+		Currency JPY = new Currency("JPY", "Japanese Yen", 78.01, 77.04);
+		Currency KRW = new Currency("KRW", "South Korean Won", 878.73, 874.15);
+		Currency EUR = new Currency("EUR", "Euro", 0.62, 0.62);
+		Currency GBP = new Currency("GBP", "British Pounds", 0.56, 0.56);
+		Currency AUD = new Currency("AUD", "Australian Dollar", 1.03, 1.02);
+		Currency NZD = new Currency("NZD", "New Zealand Dollar", 1.12, 1.10);
+		Currency IDR = new Currency("IDR", "Indonesian Rupiah", 10638.30, 10250);
+		Currency PHP = new Currency("PHP", "Philippine Peso", 38.76, 37.2);
+		Currency VND = new Currency("VND", "Vietnamese Dong", 16666.67, 0);
+		Currency INR = new Currency("INR", "Indian Rupee", 51.81, 0);
+		Currency CHF = new Currency("CHF", "Swiss Francs", 0.67, 0);
+		Currency CAD = new Currency("CAD", "Canadian Dollar", 0.97, 0.96);
 		currencyList.add(MYR);
 		currencyList.add(USD);
 		currencyList.add(THB);
@@ -43,8 +43,7 @@ public class C206_CaseStudy {
 		currencyList.add(CHF);
 		currencyList.add(CAD);
 		
-		System.out.println(String.format("%-10s: SGD$%-10.2f", "Converted Amount", currencyConverter(1000.0, currencyList)));
-		
+		searchRateByCurrency(currencyList, "NZD");
 		
 	}
 	
@@ -63,30 +62,18 @@ public class C206_CaseStudy {
 		System.out.println("Money holding added");
 	}
 	
-	public static void retrieveCurrency(ArrayList<Currency> currencyList) {
-		String output=String.format("%-10s %-10s %-10s\n", "CURRENCY", "BUY RATE", "SELL RATE");
-		for (int i=0;i<currencyList.size();i++) {
-			String cName = currencyList.get(i).getCname();
-			double buyCvalue = currencyList.get(i).getBuyCvalue();
-			double sellCvalue = currencyList.get(i).getSellCvalue();
-			output += String.format("%-10s %-10.2f %-10.2f\n", cName, buyCvalue, sellCvalue);
-		}
-		System.out.println(output);
-	}
 	
-	
-	public static void searchCurrency(ArrayList<Currency> currencyList) {
-		retrieveCurrency(currencyList);
-		String search = Helper.readString("Enter Currency to Search > ");
+	public static void searchRateByCurrency(ArrayList<Currency> currencyList, String search) {
 		
-		String output=String.format("%-10s %-10s %-10s\n", "CURRENCY", "BUY RATE", "SELL RATE");
+		String output=String.format("%-10s %-20s %-10s %-10s\n", "ISO", "CURRENCY", "BUY RATE", "SELL RATE");
 		for (int i=0;i<currencyList.size();i++) {
-			boolean isSearched = currencyList.get(i).getCname().equals(search);
+			boolean isSearched = currencyList.get(i).getIso().equals(search);
 			if (isSearched) {
-				String cName = currencyList.get(i).getCname();
-				double buyCvalue = currencyList.get(i).getBuyCvalue();
-				double sellCvalue = currencyList.get(i).getSellCvalue();
-				output += String.format("%-10s %-10.2f %-10.2f\n", cName, buyCvalue, sellCvalue);
+				String iso = currencyList.get(i).getIso();
+				String cName = currencyList.get(i).getCurrencyName();
+				double buyCvalue = currencyList.get(i).getBuyRate();
+				double sellCvalue = currencyList.get(i).getSellRate();
+				output += String.format("%-10s %-20s %-10.2f %-10.2f\n", iso, cName, buyCvalue, sellCvalue);
 				
 			}
 		}
@@ -94,86 +81,30 @@ public class C206_CaseStudy {
 		
 	}
 	
-	public static double currencyConverter(double amount, ArrayList<Currency> currencyList) {
-		displaySellOrBuyMenu();
-		double sellCvalue = 0.0;
-		double buyCvalue = 0.0;
+	public static void convertCurrency(ArrayList<Currency> currencyList) {
+		String type = Helper.readString("Enter Type of transaction > ");
+		String cIn = Helper.readString("Enter Currency In > ");
+		double amtIn = Helper.readDouble("Enter Amount In > ");
+		String cOut = Helper.readString("Enter Currency Out > ");
 		double convertedAmount = 0.0;
-		String search = "";
 		
-		int option = Helper.readInt("Enter option > ");
-		if (option == 1) {
-			displayConvertMenu();
-			int convertOption = Helper.readInt("Enter option > ");
-			if (convertOption == 1) {
-				search = Helper.readString("Convert to SGD from > ");
-				for (int i=0;i<currencyList.size();i++) {
-					boolean isSearched = currencyList.get(i).getCname().equals(search);
-					if (isSearched) {
-						sellCvalue = currencyList.get(i).getSellCvalue();
-					}
+		
+		for (int i=0;i<currencyList.size();i++) {
+			boolean isSearched = currencyList.get(i).getIso().equals(cOut);
+			if (type.equals("SELL")) {
+				if (isSearched) {
+					convertedAmount = currencyList.get(i).getSellRate()*amtIn;
+					System.out.println(String.format("SELLING %s %.2f for %s %.2f", cOut, convertedAmount, cIn, amtIn));
 				}
-				convertedAmount = amount*sellCvalue;
-				
-			} else if (convertOption == 2) {
-				search = Helper.readString("Convert from SGD to > ");
-				for (int i=0;i<currencyList.size();i++) {
-					boolean isSearched = currencyList.get(i).getCname().equals(search);
-					if (isSearched) {
-						sellCvalue = currencyList.get(i).getSellCvalue();
-					}
+			} else if (type.equals("BUY")) {
+				if (isSearched) {
+					convertedAmount = currencyList.get(i).getBuyRate()*amtIn;
+					System.out.println(String.format("BUYING %s %.2f for %s %.2f", cOut, convertedAmount, cIn, amtIn));
 				}
-				convertedAmount = amount/sellCvalue;
-			} else {
-				System.out.println("Option invalid");
-			}
-		} else if (option == 2) {
-			displayConvertMenu();
-			int convertOption = Helper.readInt("Enter option > ");
-			if (convertOption == 1) {
-				search = Helper.readString("Convert to SGD from > ");
-				for (int i=0;i<currencyList.size();i++) {
-					boolean isSearched = currencyList.get(i).getCname().equals(search);
-					if (isSearched) {
-						buyCvalue = currencyList.get(i).getBuyCvalue();
-					} 
-				}
-				convertedAmount = amount*buyCvalue;
-				
-			} else if (convertOption == 2) {
-				search = Helper.readString("Convert from SGD to > ");
-				for (int i=0;i<currencyList.size();i++) {
-					boolean isSearched = currencyList.get(i).getCname().equals(search);
-					if (isSearched) {
-						buyCvalue = currencyList.get(i).getBuyCvalue();
-					} 
-				}
-				convertedAmount = amount/buyCvalue;
-			} else {
-				System.out.println("Option invalid");
 			}
 		}
 		
-		return convertedAmount;
-		
 	}
 	
-	public static void displaySellOrBuyMenu() {
-		Helper.line(60, "=");
-		System.out.println("Currency Menu");
-		Helper.line(60, "=");
-		System.out.println("1. Sell");
-		System.out.println("2. Buy");
-		Helper.line(60, "=");
-	}
-	
-	public static void displayConvertMenu() {
-		Helper.line(60, "=");
-		System.out.println("Convert Menu");
-		Helper.line(60, "=");
-		System.out.println("1. Convert to SGD");
-		System.out.println("2. Convert from SGD");
-		Helper.line(60, "=");
-	}
 
 }
